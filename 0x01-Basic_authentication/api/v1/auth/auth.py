63788c09c4181e5d43bbf7341ea5f138fec07d15
Trevor <Trevormahinda@gmail.com>
2024-09-03 11:40:17 +0300

    pycodet

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,12 +26,12 @@ class Auth():
         """auth header"""
         if request is None:
             return None
-        
+
         header = request.headers.get("Authorization")
 
         if header is None:
             return None
-        
+
         return header
 
     def current_user(self, request=None) -> TypeVar('User'):
