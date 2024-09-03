7597be3b0e7e0721b978e34d83411e8ef1931d31
Trevor <Trevormahinda@gmail.com>
2024-09-03 11:27:08 +0300

    basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""a class to manage the API application"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class AUth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return false"""
+        if path is None:
+            return True
+        if excluded_paths == [] or excluded_paths is None:
+            return True
+        if path in excluded_paths:
+            return False
+        for p in excluded_paths:
+            if p.startswith(path):
+                return False
+            elif path.startswith(p):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """auth header"""
+        if request is None:
+            return None
+        
+        header = request.headers.get("Authorization")
+
+        if header is None:
+            return None
+        
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        return None
