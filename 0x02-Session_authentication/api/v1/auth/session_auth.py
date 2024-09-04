32e75fb8bdd4d94d29b6b429cd083b950938d6c2
Trevor <Trevormahinda@gmail.com>
2024-09-04 22:31:06 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""create session authentication system"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class sesssion Auth inherits from Auth"""
+    pass
