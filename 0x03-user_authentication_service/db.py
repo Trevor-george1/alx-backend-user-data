45353e21e233748d97203c99ba261aa0a79cbd5c
Trevor <Trevormahinda@gmail.com>
2024-09-10 12:00:43 +0300

    create_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,7 +36,7 @@ class DB:
         """saves the user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
 
-        self._session.add(user)
+        self._session.add(new_user)
         self._session.commit()
         return new_user
 
