741a19492fbf44edb85f2942c208de8495e52bf0
Trevor <Trevormahinda@gmail.com>
2024-09-10 12:07:48 +0300

    find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,14 +42,13 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """return a user based on the given arguments"""
-        for key in kwargs.keys():
-            if not hasattr(User, key):
-                raise InvalidRequestError()
-
+        if not kwargs:
+            raise NoResultFound
+        
         user = self._session.query(User).filter_by(**kwargs).first()
 
-        if user is None:
-            raise NoResultFound()
+        if not user:
+            raise NoResultFound
 
         return user
 
