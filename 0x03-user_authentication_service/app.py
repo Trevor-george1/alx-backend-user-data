369017572a778e9f2448f0a495bbbf8bdcbb0e12
Trevor <Trevormahinda@gmail.com>
2024-09-12 13:54:38 +0300

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,17 @@ def profile():
             abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_rest_password_token():
+    """creates a reset token and resets password"""
+    email = request.form.get('email')
+
+    try:
+        user_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": user_token}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
