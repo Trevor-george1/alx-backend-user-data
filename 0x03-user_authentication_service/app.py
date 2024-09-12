70a91acfc643975424e29edb940a4270673f3225
Trevor <Trevormahinda@gmail.com>
2024-09-12 12:59:08 +0300

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,5 +30,24 @@ def users():
             return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+        logs in a user and creates a session
+    """
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if AUTH.valid_login(email, password):
+            sessionID = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie('session_id', sessionID)
+
+            return response
+        else:
+            abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
