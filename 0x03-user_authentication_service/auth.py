f979b166850f0f4c1837dd7c5da4efbe76ed970f
Trevor <Trevormahinda@gmail.com>
2024-09-12 12:47:06 +0300

    session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,3 +48,12 @@ class Auth:
             return False
 
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """returns the session ID as string"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        user.session_id = _generate_uuid()
+        return user.session_id
